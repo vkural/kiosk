f92937f4d89ba65f3ce8fa4738f850d692060403
vkural <volkankural@gmail.com>
2017-05-28 05:37:37 -0700

    Fix accessing static variables

--- src/kiosk.ts
+++ src/kiosk.ts
@@ -1,5 +1,6 @@
 interface IBrowser{
     Options : any
+    Run     : any
 }
 
 abstract class Browser implements IBrowser {
@@ -10,46 +11,55 @@ abstract class Browser implements IBrowser {
         disableTextSelection  : true,
         disableTouchHighlight : true
     }
+
+    public hideCursor() : void {
+        document.body.style.cursor = 'none';
+    }
+
+    public Run() : void {
+        this.hideCursor();
+    }
 }
 
 class Chrome  extends Browser { 
-    constructor(){
+    constructor() {
         super();
     }
+
 }
 
 class Firefox extends Browser{
     constructor(){
         super();
-    }
+    }  
 }
 
-export class BrowserFactory {
+class BrowserFactory {
     private static _ua : string = (window.navigator && navigator.userAgent ) || '';
     private static _av : string = (window.navigator && navigator.appVersion) || '';
     private static _is : any = {
-      ie      : function() { return /msie/i                                           .test(this._ua); },
-      ie6     : function() { return /msie 6/i                                         .test(this._ua); },
-      ie7     : function() { return /msie 7/i                                         .test(this._ua); },
-      ie8     : function() { return /msie 8/i                                         .test(this._ua); },
-      ie9     : function() { return /msie 9/i                                         .test(this._ua); },
-      ie10    : function() { return /msie 10/i                                        .test(this._ua); },
-      ie11    : function() { return /Trident.*rv[ :]*11\./                            .test(this._ua); },
-      firefox : function() { return /firefox/i                                        .test(this._ua); },
-      gecko   : function() { return /gecko/i                                          .test(this._ua); },
-      opera   : function() { return /opera/i                                          .test(this._ua); },
-      safari  : function() { return /webkit\W(?!.*chrome).*safari\W/i                 .test(this._ua); },
-      chrome  : function() { return /webkit\W.*(chrome|chromium)\W/i                  .test(this._ua); },
-      webkit  : function() { return /webkit\W/i                                       .test(this._ua); },
-      mobile  : function() { return /iphone|ipod|(android.*?mobile)|blackberry|nokia/i.test(this._ua); },
-      tablet  : function() { return /ipad|android(?!.*mobile)/i                       .test(this._ua); },
+      ie      : function() { return /msie/i                                           .test(BrowserFactory._ua); },
+      ie6     : function() { return /msie 6/i                                         .test(BrowserFactory._ua); },
+      ie7     : function() { return /msie 7/i                                         .test(BrowserFactory._ua); },
+      ie8     : function() { return /msie 8/i                                         .test(BrowserFactory._ua); },
+      ie9     : function() { return /msie 9/i                                         .test(BrowserFactory._ua); },
+      ie10    : function() { return /msie 10/i                                        .test(BrowserFactory._ua); },
+      ie11    : function() { return /Trident.*rv[ :]*11\./                            .test(BrowserFactory._ua); },
+      firefox : function() { return /firefox/i                                        .test(BrowserFactory._ua); },
+      gecko   : function() { return /gecko/i                                          .test(BrowserFactory._ua); },
+      opera   : function() { return /opera/i                                          .test(BrowserFactory._ua); },
+      safari  : function() { return /webkit\W(?!.*chrome).*safari\W/i                 .test(BrowserFactory._ua); },
+      chrome  : function() { return /webkit\W.*(chrome|chromium)\W/i                  .test(BrowserFactory._ua); },
+      webkit  : function() { return /webkit\W/i                                       .test(BrowserFactory._ua); },
+      mobile  : function() { return /iphone|ipod|(android.*?mobile)|blackberry|nokia/i.test(BrowserFactory._ua); },
+      tablet  : function() { return /ipad|android(?!.*mobile)/i                       .test(BrowserFactory._ua); },
       desktop : function() { return !this.mobile() && !this.tablet();                                  },
-      kindle  : function() { return /kindle|silk/i                                    .test(this._ua); },
-      tv      : function() { return /googletv|sonydtv|appletv|roku|smarttv/i          .test(this._ua); },
-      windows : function() { return /win/i                                            .test(this._av); },
-      mac     : function() { return /mac/i                                            .test(this._av); },
-      unix    : function() { return /x11/i                                            .test(this._av); },
-      linux   : function() { return /linux/i                                          .test(this._av); }
+      kindle  : function() { return /kindle|silk/i                                    .test(BrowserFactory._ua); },
+      tv      : function() { return /googletv|sonydtv|appletv|roku|smarttv/i          .test(BrowserFactory._ua); },
+      windows : function() { return /win/i                                            .test(BrowserFactory._av); },
+      mac     : function() { return /mac/i                                            .test(BrowserFactory._av); },
+      unix    : function() { return /x11/i                                            .test(BrowserFactory._av); },
+      linux   : function() { return /linux/i                                          .test(BrowserFactory._av); }
     }        
 
     public static getBrowser(this: BrowserFactory) : any {
