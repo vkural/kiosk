b1262ad0cfc7877e6119d59324053e33e9d93fea
vkural <volkankural@gmail.com>
2017-05-28 06:33:40 -0700

    Add cursor hide feature

--- src/kiosk.ts
+++ src/kiosk.ts
@@ -12,8 +12,16 @@ abstract class Browser implements IBrowser {
         disableTouchHighlight : true
     }
 
-    public hideCursor() : void {
-        document.body.style.cursor = 'none';
+    public hideCursor() : any {
+        // compatibility : https://quirksmode.org/dom/core/#t14
+        let arr = <NodeListOf<HTMLElement>>document.querySelectorAll("*");
+        
+        for(var i = 0; i< arr.length; i++) {
+                // compability : https://www.quirksmode.org/dom/w3c_css.html#t46
+                arr[i].style.setProperty ("cursor", "none", "important");
+        }
+
+
     }
 
     public Run() : void {
